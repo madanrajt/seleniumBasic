ce49d5d7cd3ca0b9b72ee2b00764fd91f5283855
madanrajt <42516390+madanrajt@users.noreply.github.com>
2024-02-08 13:12:43 +0530

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,7 +1,10 @@
-import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class LoginTest {
 
@@ -9,17 +12,16 @@ public class LoginTest {
     @Test
     public  void login() throws InterruptedException {
         RemoteWebDriver driver;
-        //WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         driver.manage().window().maximize();
         Thread.sleep(3000);
-        WebElement user =  driver.findElement(By.xpath("//*[@name='username']"));
+      WebElement user =  driver.findElement(By.xpath("//*[@name='username']"));
         WebDriverWait wait = new WebDriverWait(driver,10);
-        wait.until(ExpectedConditions.visibilityOf(user));
-         driver.findElement(By.xpath("//*[@name='username']")).sendKeys("Admin");
-          driver.findElement(By.xpath("//*[@name='password']")).sendKeys("admin123");
-          driver.findElement(By.xpath("//*[@type='submit']")).click();
+        wait.until(ExpectedConditions.visibilityOf( user));
+        driver.findElement(By.xpath("//*[@name='username']")).sendKeys("Admin");
+        driver.findElement(By.xpath("//*[@name='password']")).sendKeys("admin123");
+        driver.findElement(By.xpath("//*[@type='submit']")).click();
         System.out.println("Login successful !!!!!");
         driver.close();
 
