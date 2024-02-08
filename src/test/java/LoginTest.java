e18ddb4e9d6e35800a506d6a253f049c8b445a56
madanrajt <42516390+madanrajt@users.noreply.github.com>
2024-02-08 13:06:09 +0530

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -9,14 +9,14 @@ public class LoginTest {
     @Test
     public  void login() throws InterruptedException {
         RemoteWebDriver driver;
-        WebDriverManager.chromedriver().setup();
+        //WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/validateCredentials");
         driver.manage().window().maximize();
         Thread.sleep(3000);
-        driver.findElementById("txtUsername").sendKeys("Admin");
-        driver.findElementById("txtPassword").sendKeys("admin123");
-        driver.findElementById("btnLogin").click();
+         driver.findElement(By.xpath("//*[@name='username']")).sendKeys("Admin");
+          driver.findElement(By.xpath("//*[@name='password']")).sendKeys("admin123");
+          driver.findElement(By.xpath("//*[@type='submit']")).click();
         System.out.println("Login successful !!!!!");
         driver.close();
 
