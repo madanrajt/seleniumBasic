7f4894d17d55a7db9cc9ef84935e7339fdb820f1
madanrajt <42516390+madanrajt@users.noreply.github.com>
2024-02-08 13:10:51 +0530

    Update LoginTest.java

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -11,9 +11,12 @@ public class LoginTest {
         RemoteWebDriver driver;
         //WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
-        driver.get("https://opensource-demo.orangehrmlive.com/index.php/auth/validateCredentials");
+        driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
         driver.manage().window().maximize();
         Thread.sleep(3000);
+        WebElement user =  driver.findElement(By.xpath("//*[@name='username']"));
+        WebDriverWait wait = new WebDriverWait(driver,10);
+        wait.until(ExpectedConditions.visibilityOf(user));
          driver.findElement(By.xpath("//*[@name='username']")).sendKeys("Admin");
           driver.findElement(By.xpath("//*[@name='password']")).sendKeys("admin123");
           driver.findElement(By.xpath("//*[@type='submit']")).click();
